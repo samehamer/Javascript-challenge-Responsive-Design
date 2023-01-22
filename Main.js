ab9f7d0d6e1bcc68b4cd1484593e5a24c4380c90
samehamer <108029806+samehamer@users.noreply.github.com>
2023-01-22 21:52:26 +0200

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,228 @@
+// Create Element
+let myContainer = document.createElement("div");
+let myheader = document.createElement("header");
+let myH1 = document.createElement("h2");
+let myUlist = document.createElement("ul");
+let myList1 = document.createElement("li");
+let myList2 = document.createElement("li");
+let myList3 = document.createElement("li");
+let myList4 = document.createElement("li");
+let myMain = document.createElement("main");
+
+let myProduct1 = document.createElement("div");
+let myProduct2 = document.createElement("div");
+let myProduct3 = document.createElement("div");
+let myProduct4 = document.createElement("div");
+let myProduct5 = document.createElement("div");
+let myProduct6 = document.createElement("div");
+let myProduct7 = document.createElement("div");
+let myProduct8 = document.createElement("div");
+let myProduct9 = document.createElement("div");
+let myProduct10 = document.createElement("div");
+let myProduct11 = document.createElement("div");
+let myProduct12 = document.createElement("div");
+let myProduct13 = document.createElement("div");
+let myProduct14 = document.createElement("div");
+let myProduct15 = document.createElement("div");
+
+let productH3_1 = document.createElement("h3");
+let productH3_2 = document.createElement("h3");
+let productH3_3 = document.createElement("h3");
+let productH3_4 = document.createElement("h3");
+let productH3_5 = document.createElement("h3");
+let productH3_6 = document.createElement("h3");
+let productH3_7 = document.createElement("h3");
+let productH3_8 = document.createElement("h3");
+let productH3_9 = document.createElement("h3");
+let productH3_10 = document.createElement("h3");
+let productH3_11 = document.createElement("h3");
+let productH3_12 = document.createElement("h3");
+let productH3_13 = document.createElement("h3");
+let productH3_14 = document.createElement("h3");
+let productH3_15 = document.createElement("h3");
+
+
+
+let myFooter = document.createElement("footer");
+
+myContainer.id = "container";
+
+let mylogo = document.createTextNode("sameh amer");
+let myListText1 = document.createTextNode("home");
+let myListText2 = document.createTextNode("about");
+let myListText3 = document.createTextNode("service");
+let myListText4 = document.createTextNode("contact");
+
+let productText1 = document.createTextNode("Product");
+let productText2 = document.createTextNode("Product");
+let productText3 = document.createTextNode("Product");
+let productText4 = document.createTextNode("Product");
+let productText5 = document.createTextNode("Product");
+let productText6 = document.createTextNode("Product");
+let productText7 = document.createTextNode("Product");
+let productText8 = document.createTextNode("Product");
+let productText9 = document.createTextNode("Product");
+let productText10 = document.createTextNode("Product");
+let productText11 = document.createTextNode("Product");
+let productText12 = document.createTextNode("Product");
+let productText13 = document.createTextNode("Product");
+let productText14 = document.createTextNode("Product");
+let productText15 = document.createTextNode("Product");
+
+let productH3Text1 = document.createTextNode("1");
+let productH3Text2 = document.createTextNode("2");
+let productH3Text3 = document.createTextNode("3");
+let productH3Text4 = document.createTextNode("4");
+let productH3Text5 = document.createTextNode("5");
+let productH3Text6 = document.createTextNode("6");
+let productH3Text7 = document.createTextNode("7");
+let productH3Text8 = document.createTextNode("8");
+let productH3Text9 = document.createTextNode("9");
+let productH3Text10 = document.createTextNode("10");
+let productH3Text11 = document.createTextNode("11");
+let productH3Text12 = document.createTextNode("12");
+let productH3Text13 = document.createTextNode("13");
+let productH3Text14 = document.createTextNode("14");
+let productH3Text15 = document.createTextNode("15");
+
+footerText = document.createTextNode("copyright 2021");
+
+
+// Append Element to container
+
+myContainer.appendChild(myheader);
+myheader.appendChild(myH1);
+myheader.appendChild(myUlist);
+myUlist.appendChild(myList1);
+myUlist.appendChild(myList2);
+myUlist.appendChild(myList3);
+myUlist.appendChild(myList4);
+
+myContainer.appendChild(myMain);
+
+myMain.appendChild(myProduct1);
+myMain.appendChild(myProduct2);
+myMain.appendChild(myProduct3);
+myMain.appendChild(myProduct4);
+myMain.appendChild(myProduct5);
+myMain.appendChild(myProduct6);
+myMain.appendChild(myProduct7);
+myMain.appendChild(myProduct8);
+myMain.appendChild(myProduct9);
+myMain.appendChild(myProduct10);
+myMain.appendChild(myProduct11);
+myMain.appendChild(myProduct11);
+myMain.appendChild(myProduct12);
+myMain.appendChild(myProduct13);
+myMain.appendChild(myProduct14);
+myMain.appendChild(myProduct15);
+
+
+
+myContainer.appendChild(myFooter);
+
+
+// Append Text to Element
+
+myH1.appendChild(mylogo);
+myList1.appendChild(myListText1);
+myList2.appendChild(myListText2);
+myList3.appendChild(myListText3);
+myList4.appendChild(myListText4);
+
+myProduct1.appendChild(productH3_1);
+myProduct2.appendChild(productH3_2);
+myProduct3.appendChild(productH3_3);
+myProduct4.appendChild(productH3_4);
+myProduct5.appendChild(productH3_5);
+myProduct6.appendChild(productH3_6);
+myProduct7.appendChild(productH3_7);
+myProduct8.appendChild(productH3_8);
+myProduct9.appendChild(productH3_9);
+myProduct10.appendChild(productH3_10);
+myProduct11.appendChild(productH3_11);
+myProduct12.appendChild(productH3_12);
+myProduct13.appendChild(productH3_13);
+myProduct14.appendChild(productH3_14);
+myProduct15.appendChild(productH3_15);
+
+myProduct1.appendChild(productText1);
+myProduct2.appendChild(productText2);
+myProduct3.appendChild(productText3);
+myProduct4.appendChild(productText4);
+myProduct5.appendChild(productText5);
+myProduct6.appendChild(productText6);
+myProduct7.appendChild(productText7);
+myProduct8.appendChild(productText8);
+myProduct9.appendChild(productText9);
+myProduct10.appendChild(productText10);
+myProduct11.appendChild(productText11);
+myProduct12.appendChild(productText12);
+myProduct13.appendChild(productText13);
+myProduct14.appendChild(productText14);
+myProduct15.appendChild(productText15);
+
+
+
+
+productH3_1.appendChild(productH3Text1);
+productH3_2.appendChild(productH3Text2);
+productH3_3.appendChild(productH3Text3);
+productH3_4.appendChild(productH3Text4);
+productH3_5.appendChild(productH3Text5);
+productH3_6.appendChild(productH3Text6);
+productH3_7.appendChild(productH3Text7);
+productH3_8.appendChild(productH3Text8);
+productH3_9.appendChild(productH3Text9);
+productH3_10.appendChild(productH3Text10);
+productH3_11.appendChild(productH3Text11);
+productH3_12.appendChild(productH3Text12);
+productH3_13.appendChild(productH3Text13);
+productH3_14.appendChild(productH3Text14);
+productH3_15.appendChild(productH3Text15);
+
+myFooter.appendChild(footerText);
+
+// console.log(myContainer);
+
+// Append Elemnt To Body 
+document.body.appendChild(myContainer);
+
+// styling
+
+
+
+
+myContainer.style.cssText = "box-sizing: border-box ; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; ";
+myH1.style.cssText = "display: inline-block ; margin:10px; text-transform: capitalize; color:rgb(35, 169, 110);font-size:2.2vw"; 
+myUlist.style.cssText = "display: inline-block; float:right ; font-size:20px; font-size:1.5vw"; 
+myMain.style.cssText = " background-color:rgb(236, 236, 236); width: 95% ; padding: 20px ; display:flex ; flex-wrap:wrap ;  justify-content: space-around ;  align-content: space-evenly; height: 80% ;";  
+
+
+myList1.style.cssText = "display: inline-block ; margin:10px; text-transform: uppercase; color:#BBB;"; 
+myList2.style.cssText = "display: inline-block ; margin:10px; text-transform: uppercase; color:#BBB;"; 
+myList3.style.cssText = "display: inline-block ; margin:10px; text-transform: uppercase; color:#BBB;"; 
+myList4.style.cssText = "display: inline-block ; margin:10px; text-transform: uppercase; color:#BBB;"; 
+
+myProduct1.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct2.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct3.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct4.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct5.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct6.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct7.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct8.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct9.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct10.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct11.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct12.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct13.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct14.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+myProduct15.style.cssText = "background-color:white ; text-align:center ; width:27% ; padding: 20px ; margin :10px";
+
+myFooter.style.cssText = 'background-color:rgb(35, 169, 110); color:white ; text-align:center; text-transform: capitalize ; font-size:20px ; padding:10px';
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,28 @@
+# Responsive Web Project
+
+
+## Project Title
+
+* Responsive Web Project
+
+## Table of Contents
+
+* HTML file : inedex.html
+* Javascript file : js/app.js
+* README file : README.MD
+
+## Project description  
+
+* How to make a responsive Design with JavaScript DOM Elements Only.
+
+## Technologies used to create the project 
+
+* HTML
+* CSS
+* JS
+
+
+
+
+Thanks a lot
+Sameh Amer
